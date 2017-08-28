ab9ed7c889cceba6f1610da456fd9fa96bff8393
Mathieu-WHAAAT <31046373+Mathieu-WHAAAT@users.noreply.github.com>
2017-08-28 21:24:16 +0200

    Update SnakeGame.py

--- SnakeGame.py
+++ SnakeGame.py
@@ -69,26 +69,7 @@ img_menu = pygame.transform.scale(img_menu, (int(s_screen),int(s_screen)))
 ##### CLASSES
 
             
-class Apple():
-    """ Apple object """
-    def __init__(self, Plat, nb_cases, bonus=False):
-        """ Constructor """
-        choix = Plat.cases_available(nb_cases)
-        self.pos = random.choice(choix)
 
-    def display(self, img_food, s_case):
-        """ Display the food """
-        add = (s_screen-s_board)/2
-        DISPLAYSURF.blit(img_food, (int(add+self.pos[0]*s_case), int(add+self.pos[1]*s_case)))
-
-    def set_pos(self, Plat, nb_cases):
-        """ Constructor """
-        choix = Plat.cases_available(nb_cases)
-        self.pos = random.choice(choix)
-
-    def get_pos(self):
-        """ Return Pos """
-        return self.pos
 
 
 ##### FUNCTIONS
