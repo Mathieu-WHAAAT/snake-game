7ec148bda83eba8792dd8d30a5b883056b29b220
Mathieu-WHAAAT <31046373+Mathieu-WHAAAT@users.noreply.github.com>
2017-08-28 21:19:56 +0200

    Update SnakeGame.py

--- SnakeGame.py
+++ SnakeGame.py
@@ -67,59 +67,7 @@ img_menu = pygame.transform.scale(img_menu, (int(s_screen),int(s_screen)))
 
 
 ##### CLASSES
-class Board():
-    """ Cases du tableau """
-    def __init__(self, s_board, nb_cases, size_case):
-        """ Constructor """
-        # board
-        self.board_rect = pygame.Rect(0, 0, s_board, s_board)
-        self.board_rect.centerx = s_screen//2
-        self.board_rect.centery = s_screen//2
-        # list des cases + oqp
-        self.board_case = []
-        self.case_taken = []
-        add = (s_screen-s_board)/2
-        for lig in range(nb_cases):
-            self.board_case.append([])
-            self.case_taken.append([])
-            for col in range(nb_cases):
-                self.board_case[lig].append([int(add+size_case*lig), int(add+size_case*col), int(size_case), int(size_case)])
-                self.case_taken[lig].append(False)
 
-    def get_board(self):
-        """ Return the Rect object """
-        return self.board_rect
-
-    def get_cases(self):
-        """ Return the list of cases """
-        return self.board_case
-    
-    def set_case(self, x, y):
-        """ set if case is available or not """
-        self.case_taken[x][y] = True
-        
-    def reset(self, nb_cases):
-        for lig in range(nb_cases):
-            for col in range(nb_cases):
-                self.case_taken[lig][col] = False
-
-    def cases_oqp(self, nb_cases):
-        """ """
-        oqp = []
-        for lig in range(nb_cases):
-            for col in range(nb_cases):
-                if self.case_taken[lig][col]:
-                    oqp.append((lig,col))
-        return oqp
-
-    def cases_available(self, nb_cases):
-        """ Return the cases available as a tuple """
-        available = []
-        for lig in range(nb_cases):
-            for col in range(nb_cases):
-                if not self.case_taken[lig][col]:
-                    available.append((lig, col))         
-        return available
             
 class Apple():
     """ Apple object """
