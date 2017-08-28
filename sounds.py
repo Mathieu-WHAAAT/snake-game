672fd7562be1f8acef8cc4b4a582231b2f5c8f8a
Mathieu-WHAAAT <31046373+Mathieu-WHAAAT@users.noreply.github.com>
2017-08-28 21:04:01 +0200

    Update sounds.py

--- sounds.py
+++ sounds.py
@@ -1,1 +1,12 @@
+### SOUNDS
+VOL_MUSIC = 0.6
+VOL_EFFECT= 0.6
 
+snake_grow = pygame.mixer.Sound("SOUNDS\\grow_sound.wav")
+snake_grow.set_volume(VOL_EFFECT)
+snake_die = pygame.mixer.Sound("SOUNDS\\die_sound.wav")
+snake_die.set_volume(0.7)
+
+sound_bg1 = "SOUNDS\\game_msc.mp3"
+pygame.mixer.music.load(sound_bg1)
+pygame.mixer.music.set_volume(VOL_MUSIC)
